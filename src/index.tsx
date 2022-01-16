b546b88083c1261b95d08a8cd012d89c34bb1185
Koleneko <vkoleneko@gmail.com>
2022-01-16 14:48:39 +0300

    Выполнил дз по async/await

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './tutorial-8/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from './tutorial-8/App';
+import App from "./async/App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
